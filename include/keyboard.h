84420bcdc3a6d7343d990288d3f2c0e7e2afc74f
Vítor de Godeiro Marques <vitorgodeiro@live.com>
2019-04-24 10:13:02 -0300

    Add keyboard

---
+++ include/keyboard.h
@@ -0,0 +1,206 @@
+void inputKeyboard(unsigned char key, int _x, int _y){
+	std::system("clear");
+	std::cin.clear();
+	fflush(stdin);
+	switch (key){
+		case 'a' :
+		case 'A' :
+			if (typeFrontFace == GL_CCW){
+				typeFrontFace = GL_CW;
+				glFrontFace(typeFrontFace);
+				std::cout << "Activate Clock-Wise" << std::endl;
+			}
+			else{
+				typeFrontFace = GL_CCW;
+				glFrontFace(typeFrontFace);
+				std::cout << "Activate Counter Clock-Wise" << std::endl;
+			}
+		break;
+	glFrontFace(typeFrontFace);
+		case 'c' :
+		case 'C' : {
+			std::cout << "The actual colors in RGB is:" << std::endl;
+			std::cout << "R = " << color[0] << " G = " << color[1] << " B = " << color[2] << std::endl;
+			std::cout << "Enter the new value of R:" << std::endl;
+			std::cin >> color[0];
+			std::cout << "Enter the new values of G:" << std::endl;
+			std::cin >> color[1];
+			std::cout << "Enter the new values of B" << std::endl;
+			std::cin >> color[2];
+			std::cout << "The actual colors in RGB is:" << std::endl;
+			std::cout << "R = " << color[0] << " G = " << color[1] << " B = " << color[2] << std::endl;
+			glUniform3fv(uniColor, 1, color);
+			break;}
+		case 'e' :
+		case 'E' :
+			glEnable(GL_CULL_FACE);  
+			glCullFace(GL_BACK); 
+			std::cout << "Enable Back-Face Culling" << std::endl;
+			break;
+		case 'd' :
+		case 'D' :
+			glDisable(GL_CULL_FACE);  
+			std::cout << "Disable Back-Face Culling" << std::endl;
+			break;
+		case 'f' :
+		case 'F' : {
+			std::cout << "The actual valor of Far Clipping Planes: " <<  zFar << std::endl;
+			std::cout << "Enter the new value of Far Clipping Planes:" << std::endl;
+			std::cin >> zFar;
+			std::cout << "The actual valor of Far Clipping Planes: " <<  zFar << std::endl;			
+			updateMVP();
+			break;}
+		case 'n' :
+		case 'N' : {
+			std::cout << "The actual valor of Near Clipping Planes: " <<  zNear << std::endl;
+			std::cout << "Enter the new value of Near Clipping Planes:" << std::endl;
+			std::cin >> zNear;
+			std::cout << "The actual valor of Near Clipping Planes: " <<  zNear << std::endl;			
+			updateMVP();
+			break;}
+		case 'p' :
+		case 'P' : 
+			type = GL_POINTS;
+			break;
+		case 'q' :
+		case 'Q' : 
+			exit(0);
+			break;
+		case 'r' :
+		case 'R' : {
+			float *max = mesh->box.getMax();
+			float *min = mesh->box.getMin();
+ 			r = std::sqrt(std::pow((max[0]-min[0]), 2) + std::pow((max[1]-min[1]), 2)+std::pow((max[2]-min[2]), 2));
+			fDistance =  r/tan( 30 * PI / 180.0f );
+			zNear = fDistance - r;
+			zFar = fDistance + r;
+			eye = glm::vec3(0.0f, 0.0f, fDistance);
+			lookDir = glm::vec3(0.0f, 0.0f, -1.0f);
+			up = glm::vec3(0,1,0);
+			right = glm::vec3(1.0f ,0.0f, 0.0f);
+			updateMVP();
+			break;}	
+				
+		case 't' :
+		case 'T' : 
+			glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+			type = GL_TRIANGLES;
+			break;
+		case 'w' :
+		case 'W' : 
+			glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+			type = GL_TRIANGLES;
+			break;
+/////// Roll (z)
+		case 'u' :
+		case 'U' : {
+			glm::vec3 u = right, v = up;
+			right = u*COSTHETA + v*SINTHETA;
+			up = -u*SINTHETA + v*COSTHETA;
+			updateMVP();
+			break;}
+		case 'j' :
+		case 'J' : {
+			glm::vec3 u = right, v = up;
+			right = u*COSTHETA + -v*SINTHETA;
+			up = u*SINTHETA + v*COSTHETA;
+			updateMVP();
+			break;}
+/////////// Yaw (y)
+		case 'h' :
+		case 'H' : {
+			glm::vec3 u = right, n = lookDir;
+      		right = u*COSTHETA + n*SINTHETA;
+			lookDir = -u*SINTHETA + n*COSTHETA;
+			updateMVP();
+			break;
+			}
+		case 'k' :
+		case 'K' : {
+			glm::vec3 u = right, n = lookDir;
+      		right = u*COSTHETA + -n*SINTHETA;
+			lookDir = u*SINTHETA + n*COSTHETA;
+			updateMVP();
+			break;
+			}
+/////////// Pitch (x)
+		case 'l' :
+		case 'L' :{
+			glm::vec3 v = up, n = lookDir;
+      		up = v*COSTHETA + n*SINTHETA;
+			lookDir = -v*SINTHETA + n*COSTHETA;
+			updateMVP();
+			break;
+			}
+		case 'o' :
+		case 'O' :{
+			glm::vec3 v = up, n = lookDir;
+      		up = v*COSTHETA + -n*SINTHETA;
+			lookDir = v*SINTHETA + n*COSTHETA;
+			updateMVP();
+			break;
+			}
+//
+		case '1' :
+			std:: cout << "Translate Pitch Center : " << -step << std::endl;	
+			eye += lookDir*step;
+			updateMVP();
+			break;	
+		case '2' :
+			std:: cout << "Translate Yaw Center : " << -step << std::endl;	
+			eye -= up*step;
+			lookDir = glm::normalize(-eye);
+			up = glm::normalize(glm::cross(right, lookDir));
+			updateMVP();
+			break;	
+		case '3' :
+			std:: cout << "Translate Pitch Center : " << step << std::endl;	
+			eye -= lookDir*step;
+			updateMVP();
+			break;	
+		case '4' :{
+			std:: cout << "Translate Roll Center : " << -step << std::endl;
+			eye -= right*step;
+			lookDir = glm::normalize(-eye);
+			right = glm::normalize(glm::cross(lookDir, up));
+			updateMVP();
+			break;}
+		case '6' :
+			std:: cout << "Translate Roll Center : " << step << std::endl;
+			eye += right*step;
+			lookDir = glm::normalize(-eye);
+			right = glm::normalize(glm::cross(lookDir, up));
+			updateMVP();
+			break;
+		case '8' :
+			std:: cout << "Translate Yaw Center : " << step << std::endl;	
+			eye += up*step;
+			lookDir = glm::normalize(-eye);
+			up = glm::normalize(glm::cross(right, lookDir));
+			updateMVP();
+			break;
+		case ';' :
+			std:: cout << "Translate Yaw : " << -step << std::endl;	
+			eye -= up*step;
+			updateMVP();
+			break;	
+		case ',' :{
+			std:: cout << "Translate Roll : " << -step << std::endl;
+			eye -= right*step;
+			updateMVP();
+			break;}
+		case '.' :
+			std:: cout << "Translate Roll : " << step << std::endl;
+			eye += right*step;
+			updateMVP();
+			break;
+		case '/' :
+			std:: cout << "Translate Yaw : " << step << std::endl;	
+			eye += up*step;
+			updateMVP();
+			break;
+			
+	}
+
+	glutPostRedisplay();
+}
